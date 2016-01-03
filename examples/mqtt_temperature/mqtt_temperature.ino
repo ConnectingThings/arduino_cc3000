406627feb6a1fd34fba1cd9842862bfe70233e91
Mariano Ravinale <mravinale@gmail.com>
2016-01-03 18:43:07 -0300

    updated libraries and examples

--- examples/mqtt_temperature/mqtt_temperature.ino
+++ examples/mqtt_temperature/mqtt_temperature.ino
@@ -18,9 +18,9 @@
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
 
-#define WLAN_SSID       "XXX"
-#define WLAN_PASS       "XXX"
-#define DEVICE_ID       "arduino_uno"
+#define WLAN_SSID       "WiFi-Arnet-s7fr"
+#define WLAN_PASS       "6z6n2sk9rc"
+#define DEVICE_ID       "arduino_uno" 
 
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
@@ -33,33 +33,14 @@ Adafruit_CC3000_Client client;
 // We're going to set our broker IP and union it to something we can use
 union ArrayToIp { byte array[4]; uint32_t ip; };
 
-//ArrayToIp server = { 102,1,168,192 };
-ArrayToIp server = { 168,11,170,107 };
+ArrayToIp server = { 5,1,168,192 };
+//ArrayToIp server = { 227,182,241,192 };
 
-char message_buff[50];
-const int relayPin = 6;
+char message_buff[100];
+char tempBuffer[10];
 
-void callback (char* topic, byte* payload, unsigned int length) {
-   int i = 0;
 
-  Serial.println("Message arrived:  topic: " + String(topic));
-  Serial.println("Length: " + String(length,DEC));
-  
-  for(i=0; i<length; i++) { message_buff[i] = payload[i]; }
-  message_buff[i] = '\0';
-  
-  String msgString = String(message_buff);
-  Serial.println("Payload: " + msgString);    
-  
-   if (msgString.equals("{\"value\":\"0\"}")) {
-     Serial.println("Relay OFF");
-     digitalWrite(relayPin, LOW);
-  } else if (msgString.equals("{\"value\":\"1\"}")) {
-     Serial.println("Relay ON");
-     digitalWrite(relayPin, HIGH);
-  }
-  
-}
+void callback (char* topic, byte* payload, unsigned int length) { }
 cc3000_PubSubClient mqttclient(server.ip, 1883, callback, client, cc3000);
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -67,9 +48,7 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup(void)
 {
   Serial.begin(115200);
-  
-  pinMode(relayPin, OUTPUT);
-  
+   
   Serial.println(F("Initialising the CC3000 ..."));
   if (!cc3000.begin()) {  Serial.println(F("Unable to initialise the CC3000! Check your wiring?")); while(1); }
  
@@ -93,22 +72,33 @@ void setup(void)
 }
 
 void loop(void) {
-       
+   
+ float t = dht.readTemperature(); 
+ String temp = dtostrf(t, 5, 2, tempBuffer);
+  
+String messageJson = "{\"sensors\":[{\"value\":\""+temp+"\",\"tag\":\"temperature\"}]}"; 
+ messageJson.toCharArray(message_buff, 100);
+
+ 
+ Serial.println("message to send: "+messageJson); 
   // are we still connected?
   if (!client.connected()) {
-     Serial.println(F("trying to connect to the internet..."));
+     Serial.println(F("trying to connect to the broker..."));
      client = cc3000.connectTCP(server.ip, 1883);
      
      if(client.connected()) {
-       Serial.println(F("connected to the internet!"));
+       Serial.println(F("connected to the broker!"));
        if (mqttclient.connect(DEVICE_ID)) { 
-          Serial.println(F("connected to the mqtt broker!"));
-          mqttclient.subscribe("/arduino_uno/relay");
+         Serial.println(F("connected to the mqtt broker!"));
        }
      } 
-  } 
+  } else {
+    // yep, publish that test  
+    mqttclient.publish("/device/arduino_uno/key/1qaz2wsx", message_buff); 
+  }
 
   mqttclient.loop();
-  delay(10000);
+  delay(5000);
 }
+
 
