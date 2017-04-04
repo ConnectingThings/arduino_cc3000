e2bc5170789dbc3335b9d1662dbf2a30ec88ce9b
Mariano Ravinale <mravinale@gmail.com>
2017-04-03 23:25:57 -0300

    updated to latest version

--- examples/mqtt_pir/mqtt_pir.ino
+++ examples/mqtt_pir/mqtt_pir.ino
@@ -7,22 +7,22 @@ written by Mariano Ravinale
 #include <ccspi.h>
 #include <SPI.h>
 #include <PubSubClient.h>
- 
+
 #define aref_voltage 3.3
 
-#define ADAFRUIT_CC3000_IRQ   3  
+#define ADAFRUIT_CC3000_IRQ   3
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
- 
+
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
- 
+
 #define WLAN_SSID       "WiFi-SSID"
 #define WLAN_PASS       "Password"
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 char humidityBuffer[50];
 char tempBuffer[50];
- 
+
 Adafruit_CC3000_Client client = Adafruit_CC3000_Client();
 PubSubClient mqttclient("app.connectingthings.io", 1883, client);
 
@@ -33,43 +33,43 @@ int val = 0;                    // variable for reading the pin status
 void setup(void)
 {
  Serial.begin(115200);
-   
+
   Serial.println(F("Initialising the CC3000 ..."));
   if (!cc3000.begin()) {  Serial.println(F("Failed!")); while(1); }
- 
+
   Serial.println(F("Deleting old connection profiles"));
   if (!cc3000.deleteProfiles()) {  Serial.println(F("Failed!"));  while(1);  }
 
-  char *ssid = WLAN_SSID; 
+  char *ssid = WLAN_SSID;
   Serial.println(F("Attempting to connect to ")); Serial.println(ssid);
-  
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { Serial.println(F("Failed!")); while(1); }   
+
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { Serial.println(F("Failed!")); while(1); }
   Serial.println(F("Connected!"));
-  
+
   Serial.println(F("Request DHCP"));
   while (!cc3000.checkDHCP()) { delay(100);}
   displayConnectionDetails();
-   
-  pinMode(inputPin, INPUT);     // declare sensor as input 
+
+  pinMode(inputPin, INPUT);     // declare sensor as input
 }
- 
+
 void loop(void) {
 
   if (!mqttclient.connected()) {  reconnect(); }
 
   val = digitalRead(inputPin);  // read input value
-  
-  if (val == HIGH && pirState == LOW ) {     
-    Serial.println("Motion detected!");     
-    mqttclient.publish("device/switch/key/xxxxxxx","{\"value\": \"1\",\"tag\": \"movement\"}", true);
-    pirState = HIGH; 
-    delay(10*1000); //10 seconds on hold   
-  } else if (pirState == HIGH) {        
-    Serial.println("Motion ended!"); 
-    mqttclient.publish("device/switch/key/xxxxxxx","{\"value\": \"0\",\"tag\": \"movement\"}", true);
-    pirState = LOW;    
+
+  if (val == HIGH && pirState == LOW ) {
+    Serial.println("Motion detected!");
+    mqttclient.publish("key/xxxxxxx/device/switch/tag/movement","{\"value\": \"1\"}", true);
+    pirState = HIGH;
+    delay(30*1000); //10 seconds on hold   
+  } else if (pirState == HIGH) {
+    Serial.println("Motion ended!");
+    mqttclient.publish("key/xxxxxxx/device/switch/tag/movement","{\"value\": \"0\"}", true);
+    pirState = LOW;
   }
- 
+
   mqttclient.loop();
 }
 
@@ -81,18 +81,18 @@ void loop(void) {
 void reconnect() {
   // Loop until we're reconnected
   while (!mqttclient.connected()) {
-    Serial.println("Attempting MQTT connection...");   
+    Serial.println("Attempting MQTT connection...");
     if (mqttclient.connect("arduinoClient")) {
-      Serial.print("Connected!");       
+      Serial.print("Connected!");
     } else {
       Serial.print("Failed!");
       Serial.print(mqttclient.state());
-      Serial.println("trying again in 5 seconds"); 
+      Serial.println("trying again in 5 seconds");
       delay(5000);
     }
   }
 }
- 
+
 /**************************************************************************/
 /*!
     @brief  Tries to read the IP address
@@ -101,7 +101,7 @@ void reconnect() {
 bool displayConnectionDetails(void)
 {
   uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  
+
   if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
   {
     Serial.println(F("Unable to retrieve the IP Address!\r\n"));
@@ -109,7 +109,7 @@ bool displayConnectionDetails(void)
   }
   else
   {
-    Serial.print(F("IP Address: ")); cc3000.printIPdotsRev(ipAddress); 
+    Serial.print(F("IP Address: ")); cc3000.printIPdotsRev(ipAddress);
     Serial.println();
     return true;
   }
--- examples/mqtt_temperature/mqtt_temperature.ino
+++ examples/mqtt_temperature/mqtt_temperature.ino
@@ -8,25 +8,25 @@ written by Mariano Ravinale
 #include <SPI.h>
 #include <DHT.h>
 #include <PubSubClient.h>
- 
+
 #define aref_voltage 3.3
 
-#define ADAFRUIT_CC3000_IRQ   3  
+#define ADAFRUIT_CC3000_IRQ   3
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
- 
+
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
- 
-#define WLAN_SSID       "WiFi-SSID"
-#define WLAN_PASS       "Password"
+
+#define WLAN_SSID       "SkyNet"
+#define WLAN_PASS       "yanina2708"
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 #define DHTPIN 7     // what pin we're connected to
-#define DHTTYPE DHT11   
+#define DHTTYPE DHT11
 
 char humidityBuffer[50];
 char tempBuffer[50];
- 
+
 Adafruit_CC3000_Client client = Adafruit_CC3000_Client();
 PubSubClient mqttclient("app.connectingthings.io", 1883, client);
 
@@ -35,54 +35,54 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup(void)
 {
  Serial.begin(115200);
-   
+
   Serial.println(F("Initialising the CC3000 ..."));
   if (!cc3000.begin()) {  Serial.println(F("Failed!")); while(1); }
- 
+
   Serial.println(F("Deleting old connection profiles"));
   if (!cc3000.deleteProfiles()) {  Serial.println(F("Failed!"));  while(1);  }
 
-  char *ssid = WLAN_SSID; 
+  char *ssid = WLAN_SSID;
   Serial.println(F("Attempting to connect to ")); Serial.println(ssid);
-  
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { Serial.println(F("Failed!")); while(1); }   
+
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) { Serial.println(F("Failed!")); while(1); }
   Serial.println(F("Connected!"));
-  
+
   Serial.println(F("Request DHCP"));
   while (!cc3000.checkDHCP()) { delay(100);}
   displayConnectionDetails();
-  
-  dht.begin(); 
+
+  dht.begin();
 }
- 
+
 void loop(void) {
 
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  
+
   char tempBuffer[50];
   String temp = dtostrf(t, 5, 2, tempBuffer);
-  
+
   char humidityBuffer[50];
   String humidity = dtostrf(h, 5, 2, humidityBuffer);
- 
-  String humidityJson = "{\"value\": \""+humidity+"\",\"tag\": \"humidity\"}";
+
+  String humidityJson = "{\"value\": \""+humidity+"\"}";
   humidityJson.toCharArray(humidityBuffer, 50);
- 
-  String temperatureJson = "{\"value\": \""+temp+"\",\"tag\": \"temperature\"}";
+
+  String temperatureJson = "{\"value\": \""+temp+"\"}";
   temperatureJson.toCharArray(tempBuffer, 50);
-    
+
   // are we still connected?
   if (!mqttclient.connected()) {  reconnect(); }
-      
-  Serial.println("Temperature: "+temp); 
-  mqttclient.publish("/device/arduino_uno/key/xxxxxxx",tempBuffer);
-  delay(5000);
-  Serial.println("Humidity: "+humidity); 
-  mqttclient.publish("/device/arduino_uno/key/xxxxxxx",humidityBuffer);
-    
+
+  Serial.println("Temperature: "+temp);
+  mqttclient.publish("key/1qaz2wsx/device/arduino/tag/temperature",tempBuffer, true);
+  delay(30000);
+  Serial.println("Humidity: "+humidity);
+  mqttclient.publish("key/1qaz2wsx/device/arduino/tag/humidity",humidityBuffer, true);
+
   mqttclient.loop();
-  delay(5000);
+  delay(30000);
 }
 
 /**************************************************************************/
@@ -93,18 +93,18 @@ void loop(void) {
 void reconnect() {
   // Loop until we're reconnected
   while (!mqttclient.connected()) {
-    Serial.println("Attempting MQTT connection...");   
+    Serial.println("Attempting MQTT connection...");
     if (mqttclient.connect("arduinoClient")) {
-      Serial.print("Connected!");       
+      Serial.print("Connected!");
     } else {
       Serial.print("Failed!");
       Serial.print(mqttclient.state());
-      Serial.println("trying again in 5 seconds"); 
-      delay(5000);
+      Serial.println("trying again in 10 seconds");
+      delay(10000);
     }
   }
 }
- 
+
 /**************************************************************************/
 /*!
     @brief  Tries to read the IP address and other connection details
@@ -113,7 +113,7 @@ void reconnect() {
 bool displayConnectionDetails(void)
 {
   uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  
+
   if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
   {
     Serial.println(F("Unable to retrieve the IP Address!\r\n"));
